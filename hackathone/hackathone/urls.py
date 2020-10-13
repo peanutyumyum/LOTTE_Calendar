5ef62e5f5e917ac27b2b8d56b98d6f1b0e688a0e
peanutyumyum <jrimit@gmail.com>
2020-10-14 00:54:18 +0900

    make clanedar_app for work place

--- hackathone/hackathone/urls.py
+++ hackathone/hackathone/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
+    path('calendar/', include('calendar_app.urls', namespace='calendar_app')),
     path('login/', include('login.urls', namespace='login')),
 ]
