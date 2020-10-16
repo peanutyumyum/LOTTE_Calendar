37bfc10e2b13e0d3a96996b4700a9127cece36bd
aeuna <4294psy@naver.com>
2020-10-16 23:21:22 +0900

    Add calendarTestApp

---
+++ project/calendarTestProj/testApp/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
+
+
+class CalendarEvent(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, blank=True, related_name="schedules")
+    title = models.CharField(_('Title'), max_length=200)
+    start = models.DateTimeField(_('Start'))
+    end = models.DateTimeField(_('End'))
+    all_day = models.BooleanField(_('All day'), default=False)
+    # models.DateTimeField(input_formats=["%d %b %Y %H:%M:%S %Z"])
+    class Meta:
+        verbose_name = _('Event')
+        verbose_name_plural = _('Events')
+
+        ordering = ['-start']
+
+    # 관리자 사이트에 표시될 객체 이름 설정 
+    def __str__(self):
+        return self.title
