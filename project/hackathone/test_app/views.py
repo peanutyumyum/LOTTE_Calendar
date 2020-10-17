2957921409a58ba6bf51b433760e402dccd9a212
peanutyumyum <jrimit@gmail.com>
2020-10-17 23:30:40 +0900

    calendar function cmpleted

--- project/hackathone/test_app/views.py
+++ project/hackathone/test_app/views.py
@@ -6,10 +6,3 @@ import calendar
 
 def test(request):
     return render(request, 'test.html')
-
-htmlCalendar = calendar.HTMLCalendar(calendar.SUNDAY)
-s = htmlCalendar.formatmonth(2020,10)
-print(s)
-date =[]
-for i in date:
-    mark = "<script src='%s'></script>" % i
