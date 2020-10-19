cde69aeea59f9cafbb7396daba74e50d628b8157
peanutyumyum <jrimit@gmail.com>
2020-10-19 23:19:14 +0900

    edit detail

--- project/hackathone/calendar_app/views.py
+++ project/hackathone/calendar_app/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.shortcuts import render
 from .models import CalendarEvent
 
@@ -7,5 +6,10 @@ def calendar(request):
     events = CalendarEvent.objects.all()
     space = "&nbsp;"
     long_space = space*10
+    for i in events:
+        a = str(i.start)[0:10]
+    print(a)
 
     return render(request,'calendar.html',{'events':events, 'space' : space, 'long_spcae' : long_space})
+
+    # 2020-10-05 07:14:32+00:00
