17d7e4cc71f044b32d04c3538a51e1f5bdb516fb
peanutyumyum <jrimit@gmail.com>
2020-10-20 01:18:10 +0900

    calendar time function edited

--- project/hackathone/calendar_app/views.py
+++ project/hackathone/calendar_app/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
 from .models import CalendarEvent
+import datetime
 
 # Create your views here.
 def calendar(request):
     events = CalendarEvent.objects.all()
     space = "&nbsp;"
     long_space = space*10
-    for i in events:
-        a = str(i.start)[0:10]
-    print(a)
+    
+    def detail_information_render(today_item):
+        arr = []
+        detail_events = CalendarEvent.objects.get(pk=today_item)
+        for i in detail_events:
+            arr.append(i)
+        return arr
+    
 
-    return render(request,'calendar.html',{'events':events, 'space' : space, 'long_spcae' : long_space})
+
+    for i in events:
+        today = datetime.datetime.now()
+        start_day = i.start
+        end_day = i.end
+        if today > start_day:
+            if today < end_day:
+                a = detail_information_render(i.id)
+                
+
+    return render(request,'calendar.html',{'events':events, 'space' : space, 'long_spcae' : long_space, 'detail_events' : a})
 
     # 2020-10-05 07:14:32+00:00
