fc41f57c04905139aa22bfda0cc5c6ce59858b5d
glossyyoon <mypju@naver.com>
2020-10-25 21:58:06 +0900

    heroku

--- project/hackathone/test_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'test_app'
-
-urlpatterns=[
-    path('', views.test, name='test'),
-]
--- project/hackathone/test_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-import calendar
-
-
-# Create your views here.
-
-def test(request):
-    return render(request, 'test.html')
